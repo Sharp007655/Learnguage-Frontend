cd7c064e39034dc29a57525a79ba8b1630403b18
Soma Ando <soma.ando.1227@gmail.com>
2024-08-23 23:53:51 +0900

    Add file

--- src/app/page.js
+++ src/app/page.js
@@ -2,52 +2,51 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import liff from '@line/liff';
 
-export default function Home({ liff, liffError }) {
+export default function Home() {
   const [idToken, setIdToken] = useState(null);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    if (liff) {
-      liff.ready.then(() => {
-        liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
-          .then(() => {
-            console.log("LIFF init succeeded.");
-            const token = liff.getIDToken();
-            console.log("ID Token:", token);
-            setIdToken(token);
-          })
-          .catch((error) => {
-            console.error('LIFF init failed', error);
-          });
-      }).catch((error) => {
-        console.error('LIFF ready failed', error);
+    // liff の初期化
+    liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
+      .then(() => {
+        console.log("LIFF init succeeded.");
+        if (liff.isLoggedIn()) {
+          const token = liff.getIDToken();
+          console.log("ID Token:", token);
+          setIdToken(token);  // idTokenが更新される
+        } else {
+          liff.login();
+        }
+      })
+      .catch((error) => {
+        console.error('LIFF init failed', error);
       });
-    }
-  }, [liff]);
+  }, []);
 
+  // idTokenが取得されたらAPIリクエストを実行
   useEffect(() => {
-    axios.get(`https://api.learnguage.somando.jp/user/dictionary`, {
-      method: "GET",
-    })
-    .then(response => {
-      console.log('API response:', response.data);
-      setData(response.data["words"]); // APIから取得したデータを状態に保存
-    })
-    .catch(error => {
-      console.error('API call failed:', error);
-    });
+    if (idToken) {  // idTokenがnullではないことを確認
+      axios.get(`https://api.learnguage.somando.jp/user/dictionary?idToken=${idToken}`)
+        .then(response => {
+          console.log('API response:', response.data.words);
+          setData(response.data.words);  // APIから取得したデータを状態に保存
+        })
+        .catch(error => {
+          console.error('API call failed:', error);
+        });
+    }
   }, [idToken]);
 
   return (
-    <div>
-      <h1>APIから取得したデータ</h1>
-      {data}
-      <ul>
-        {data.map((item) => (
-          <li>{item}</li> // ここでデータをHTMLに並べる
-        ))}
-      </ul>
-    </div>
+    <main>
+      <h1>Dictionary</h1>
+      <p>{idToken}</p>
+        {data.map((item) => 
+          <p>{item["word"]}, {item["mean"]}, {item["read"]}</p>
+        )}
+    </main>
   );
 }
